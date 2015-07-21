cf24ca8b8311661e1d8bf7ba2da2542c08f860f8
Adrian Mouat <adrian.mouat@gmail.com>
2015-07-21 23:14:57 +0100

    Support htmlbook. I AM A GENIUS.
    Should have called it asiline. Missed a trick there.

--- assline.py
+++ assline.py
@@ -1,22 +1,35 @@
 import sys
 
 
-def ass(fname):
+def ass(fname, type="docbook"):
 
     in_code = False
     in_cont = False
+
+    open_tag = "<screen>"
+    close_tag = "</screen>"
+
+    in_open_tag = "<userinput>"
+    in_close_tag = "</userinput>"
+
+    if type == "htmlbook":
+        open_tag = "<pre>"
+        close_tag = "</pre>"
+
+        in_open_tag = "<strong>"
+        in_close_tag = "</strong>"
 
     with open(fname) as f:
         for line in f:
             if line[:4] == ",,,,":
                 if not in_code:
                     print("++++")
-                    print("<screen>")
+                    print(open_tag)
                     in_code = True
                     in_cont = False
                     continue
                 else:
-                    print("</screen>")
+                    print(close_tag)
                     print("++++")
                     in_code = False
                     in_cont = False
@@ -27,17 +40,17 @@ def ass(fname):
                     print(line, end='')
                     continue
                 else:
-                    print(line[2:-1], "</userinput>", sep='')
+                    print(line[2:-1], in_close_tag, sep='')
                     in_cont = False
                     continue
 
             if in_code and line[:2] == "$ ":
                 if line[-2:-1] == "\\":
-                    print("$ <userinput>", line[2:-1], sep='')
+                    print("$ ", in_open_tag, line[2:-1], sep='')
                     in_cont = True
                     continue
                 else:
-                    print("$ <userinput>", line[2:-1], "</userinput>", sep='')
+                    print("$ ", in_open_tag, line[2:-1], in_close_tag, sep='')
                     continue
             else:
                 print(line, end='')
